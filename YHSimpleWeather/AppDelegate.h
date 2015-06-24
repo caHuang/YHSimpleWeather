081798f44fe59572e3b83cdf017fc4ccf04f50cd
Yolk Huang <yolk.tw@gmail.com>
2015-06-24 11:27:11 +0800

    Add base layout on ViewController

--- YHSimpleWeather/AppDelegate.h
+++ YHSimpleWeather/AppDelegate.h
@@ -10,6 +10,6 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (nonatomic, strong) UIWindow *window;
+@property (strong, nonatomic) UIWindow *window;
 
 @end
--- YHSimpleWeather/ViewController.m
+++ YHSimpleWeather/ViewController.m
@@ -6,22 +6,76 @@
 //  Copyright (c) 2015 Yolk Huang. All rights reserved.
 //
 
+#import <LBBlurredImage/UIImageView+LBBlurredImage.h>
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <
+  UITableViewDelegate,
+  UITableViewDataSource,
+  UIScrollViewDelegate
+>
+
+@property (strong, nonatomic) UIImageView *backgroundImageView;
+@property (strong, nonatomic) UIImageView *blurredImageView;
+@property (strong, nonatomic) UITableView *tableView;
+
+- (void)setupBackgroundAndBlurredImageView;
+- (void)setupTableView;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - Lifecycle
+
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+  [super viewDidLoad];
+  [self setupBackgroundAndBlurredImageView];
+  [self setupTableView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Private
+
+- (void)setupBackgroundAndBlurredImageView {
+  UIImage *backgroundImage = [UIImage imageNamed:@"bg"];
+  
+  self.backgroundImageView = [[UIImageView alloc] initWithImage:backgroundImage];
+  self.backgroundImageView.contentMode = UIViewContentModeScaleAspectFill;
+  [self.view addSubview:self.backgroundImageView];
+  
+  self.blurredImageView = [[UIImageView alloc] init];
+  self.blurredImageView.contentMode = UIViewContentModeScaleAspectFill;
+  self.blurredImageView.alpha = 0;
+  [self.blurredImageView setImageToBlur:backgroundImage
+                             blurRadius:10
+                        completionBlock:nil];
+  [self.view addSubview:self.blurredImageView];
+}
+
+- (void)setupTableView {
+  self.tableView = [[UITableView alloc] init];
+  self.tableView.backgroundColor = [UIColor clearColor];
+  self.tableView.delegate = self;
+  self.tableView.dataSource = self;
+  self.tableView.separatorColor = [UIColor colorWithWhite:1 alpha:0.2];
+  self.tableView.pagingEnabled = YES;
+  [self.view addSubview:self.tableView];
+}
+
+#pragma mark - UIViewController
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+  return UIStatusBarStyleLightContent;
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  return nil;
 }
 
 @end
